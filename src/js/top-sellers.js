4e79b5680d923c0afa47f53517f383a9d0e64558
Merylly <142231027+Merylly@users.noreply.github.com>
2024-02-23 12:29:40 +0200

    add html, css and js

--- src/js/top-sellers.js
+++ src/js/top-sellers.js
@@ -12,3 +12,43 @@ const getTopBooksData = async () => {
   /* renderTopBooks(topBooks); */
 };
 getTopBooksData();
+
+// markup
+
+function createMarkUp(results) {
+  const markUp = results.map(
+    ({ list_name, books: { book_image, title, author, _id } }) => {
+      `<li class="sellers-category">
+        <h2 class="sellers-category-title">${list_name}</h2>
+        <ul class="sellers-category-list">
+            <li class="sellers-item" data-id="${_id}">
+                <img class="book-image" src="${book_image}" alt="${title}">
+                <h3 class="book-title">${title}</h3>
+                <p class="book-author">${author}</p>
+            </li>
+            <li class="sellers-item" data-id="${_id}">
+                <img class="book-image" src="${book_image}" alt="${title}">
+                <h3 class="book-title">${title}</h3>
+                <p class="book-author">${author}</p>
+            </li>
+            <li class="sellers-item" data-id="${_id}">
+                <img class="book-image" src="${book_image}" alt="${title}">
+                <h3 class="book-title">${title}</h3>
+                <p class="book-author">${author}</p>
+            </li>
+            <li class="sellers-item" data-id="${_id}">
+                <img class="book-image" src="${book_image}" alt="${title}">
+                <h3 class="book-title">${title}</h3>
+                <p class="book-author">${author}</p>
+            </li>
+            <li class="sellers-item" data-id="${_id}">
+                <img class="book-image" src="${book_image}" alt="${title}">
+                <h3 class="book-title">${title}</h3>
+                <p class="book-author">${author}</p>
+            </li>
+        </ul>
+      </li>`;
+    }
+  );
+  return markUp;
+}
