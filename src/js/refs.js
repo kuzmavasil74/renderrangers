fb0619241332ac6c6c37c7e33773c033ba8ba195
AnnaZabolotnia <137837327+AnnaZabolotnia@users.noreply.github.com>
2024-02-24 19:22:18 +0200

    support

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,8 @@ export const refs = {
   /* Після створкння розмітки тут прописуємо свії посилання на елементи в котрі буде вставлятись розмітка */
 
   categoryListElem: document.querySelector('.js-category-list'),
-  supportList: document.querySelector('.js-support-list'),
+  supportList: document.querySelector('.swiper-wrapper'),
+  scrollBtn: document.querySelector('.js-support-btn'),
+  scrollToTopBtn: document.querySelector('.js-scrollToTopBtn'),
   /* loadElem: document.querySelector('.js-loader'), */
 };
--- src/js/support-section.js
+++ src/js/support-section.js
@@ -1,30 +1,89 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 import { supportList } from '../db/supportList';
 import { refs } from './refs';
 
+// 1. appending list items to the support list
 renderSupportList(supportList);
 
-function supportTemplate(item) {
+function renderSupportList(supportList) {
+  const listItems = supportList.map(createSupportItem);
+  refs.supportList.append(...listItems);
+}
+
+function createSupportItem(item, index) {
+  // example index = 0
+  // example index = 1
+  // example index = 2
+  // example: item =  {
+  //   title: 'Save the Children',
+  //   url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+  //   img: 'stc',
+  // },
+
   //що там в item
   const { title, url, img } = item;
-  console.log(item);
-  return `
-    <li class="support-item">
-      <a
-        class="support-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        arial-label="${title}"
-        href="${url}"
-      >
-        <img class="support-img" src="${img}" alt="${title}" />
-      </a>
-    </li>`;
-}
-function supportListTemplate(supportList) {
-  return supportList.map(supportTemplate).join('');
-}
-function renderSupportList(supportList) {
-  const markup = supportListTemplate(supportList);
+  //create li
+  const li = document.createElement('li');
+  li.classList.add('swiper-slide');
+  // create a
+  const a = document.createElement('a');
+  a.classList.add('support-link');
+  a.target = '_blank';
+  a.rel = 'noopener noreferrer';
+  a.ariaLabel = title;
+  a.href = url;
+  // create count
+  const span = document.createElement('span');
+  span.classList.add('support-count');
+  span.textContent = `0${index + 1}`;
+  // create img
+  const imgEl = document.createElement('img');
+  imgEl.classList.add('support-img');
+  imgEl.src = `./img/support/${img}.png`;
+  imgEl.alt = title;
 
-  refs.supportList.insertAdjacentHTML('beforeend', markup);
+  // add count and image in a
+  a.append(span, imgEl);
+  // add a in li
+  li.append(a);
+  // return li
+  return li;
 }
+
+// 2. Setting up swiper
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+const swiper = new Swiper('.support-swiper', {
+  direction: 'vertical',
+  slidesPerView: 6,
+  slidesPerGroup: 6,
+  // can be true but will turn button into disable
+  // can be avoided by changing html button to div/span
+  allowTouchMove: false,
+  spaceBetween: '20px',
+
+  // configure Swiper to use modules
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.next-btn',
+    prevEl: '.prev-btn',
+    disabledClass: 'none',
+  },
+  on: {
+    slideChange: function () {
+      if (this.activeIndex === 0) {
+        prevBtn.classList.add('swiper-button-hidden');
+        nextBtn.classList.remove('swiper-button-hidden');
+      } else {
+        prevBtn.classList.remove('swiper-button-hidden');
+        nextBtn.classList.add('swiper-button-hidden');
+      }
+    },
+  },
+});
