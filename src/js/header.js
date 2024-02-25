572d2cd2297a826d8cc043f6923629f1a42cb3a5
AnnaP-G <klientkv1511@gmail.com>
2024-02-25 09:16:23 +0200

    header8

--- src/js/header.js
+++ src/js/header.js
@@ -13,5 +13,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const burgerMenuBtn = document.querySelector('.burger-menu-btn');
+//     const closeMenuBtn = document.querySelector('.close-menu-btn');
+//     const headerNav = document.querySelector('.header-nav');
+
+//     burgerMenuBtn.addEventListener('click', function () {
+//         burgerMenuBtn.classList.add('is-hidden');
+//         closeMenuBtn.classList.remove('is-hidden');
+
+//         headerNav.style.display = 'flex';
+//     });
+
+//     closeMenuBtn.addEventListener('click', function () {
+//         burgerMenuBtn.classList.remove('is-hidden');
+//         closeMenuBtn.classList.add('is-hidden');
+
+//         headerNav.style.display = 'none';
+//     });
+// });
+
+
+
 
 
